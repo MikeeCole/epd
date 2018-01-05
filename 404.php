87dffe780671eb95ff641d1673926262d736d175
Michael Cole <mcole@eclipsepd.com>
2018-01-05 13:05:52 -0500

    added 404

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php /* Template Name: Plain Template */ ?>
+
+
+<?php get_header(); ?>
+<!-- plain.php start -->
+<section>
+    <h2>Page Not Found</h2>
+    Hmm, that page doesn't seem to be here. Maybe something else will interest you.
+</section>
+
+<section>
+    <?php get_sidebar(); ?>
+</section>
+<!-- plain.php end -->
+
+<?php get_footer(); ?>
